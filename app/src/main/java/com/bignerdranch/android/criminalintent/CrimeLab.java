a4f15560de3804893155f016f4f17db0d0ab2497
Crush <art60@rambler.ru>
2020-10-07 08:42:53 +0300

    Код главы 9

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -21,7 +21,7 @@ public class CrimeLab {
         mCrimes = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
+            crime.setTitle("Преступление #" + i);
             crime.setSolved(i % 2 == 0); // Для каждого второго объекта
             mCrimes.add(crime);
         }
