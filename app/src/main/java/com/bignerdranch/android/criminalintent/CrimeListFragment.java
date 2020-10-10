275c5c7baf36195c2dd02856c4122bf0eb4f1e94
Crush <art60@rambler.ru>
2020-10-10 14:04:25 +0300

    Упражнение главы 10. Эффективная перезагрузка RecyclerView

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
