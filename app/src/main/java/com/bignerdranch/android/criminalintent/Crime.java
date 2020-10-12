1558edfe554dd4054256e49d60d17f01e5db0032
Crush <art60@rambler.ru>
2020-10-12 21:12:35 +0300

    Revert "Код главы 12"
    This reverts commit 197ad5cc

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -8,7 +8,6 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
-    private boolean mRequiresPolice;
 
     public Crime() {
         mId = UUID.randomUUID();
@@ -41,13 +40,5 @@ public class Crime {
 
     public void setSolved(boolean solved) {
         mSolved = solved;
-    }
-
-    public boolean isRequiresPolice() {
-        return mRequiresPolice;
-    }
-
-    public void setRequiresPolice(boolean requiresPolice) {
-        mRequiresPolice = requiresPolice;
     }
 }
