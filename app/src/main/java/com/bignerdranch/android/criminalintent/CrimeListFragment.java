dddcf13574b456542978676146ef1da4e7eff024
Crush <art60@rambler.ru>
2020-10-12 19:03:46 +0300

    Упражение главы 11. Добавление кнопок для перехода в начало и конец списка

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
