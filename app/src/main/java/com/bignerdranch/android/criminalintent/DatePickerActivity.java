07ce62e4e4858ac964138fd6c4ef76cb22bb4507
Crush <art60@rambler.ru>
2020-10-15 21:21:00 +0300

    Упражнение главы 12. DialogFragment

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerActivity.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+public class DatePickerActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new DatePickerFragment();
+    }
+}
